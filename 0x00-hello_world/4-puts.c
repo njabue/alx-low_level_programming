8a117a18344c1b9e3eac6d467a31b6afb39cbba7
njabu-loe <njastk5@gmail.com>
2023-03-20 00:20:01 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - print the string in the put function
+ * this program prints "Programming is like building a multiple puzzle"
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multiple puzzle\n");
+	puts("\"Programming is like building a multiple puzzle");
 	return (0);
 }
