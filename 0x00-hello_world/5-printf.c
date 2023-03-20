24b2a2232e747a724fad821e76d6888a5425e6e0
njabu-loe <njastk5@gmail.com>
2023-03-20 17:40:37 +0200

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the printf function
+ *main - print the string in the printf function
  *
- * Descrption using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art,"
- * Return: 0
+ *Descrption using the main function
+ *this program prints a string
+ *Return:0
  */
 int main(void)
 {
