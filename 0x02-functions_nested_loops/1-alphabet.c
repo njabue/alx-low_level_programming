280e733a841c795a0d70c4831694faa89b654361
njabu-loe <njastk5@gmail.com>
2023-03-23 03:00:10 +0200

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_alphabet - the alphabet, in lowercase, followed by a new line.
+* Return: Always 0 (Success)
+*
+*/
+
+
+void print_alphabet(void)
+
+{
+char n;
+for (n = 'a'; n <= 'z'; n++)
+_putchar(n);
+
+_putchar('\n');
+}
